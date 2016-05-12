63f2b7658261ad323845ad6afd3c588034a7ed00
Kushan Joshi <0o3ko0@gmail.com>
2016-05-12 15:49:15 +0530

    base

---
+++ app/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+export default class ere extends React.Component {
+  render() {
+    return (
+      <h3>hefssllo</h3>
+    );
+  }
+}
---
+++ app/index.js
@@ -0,0 +1,9 @@
+import { render } from 'react-dom';
+import React from 'react';
+import App from './App';
+const node = document.getElementById('app');
+// const X = () => <h2>wea  sslloa</h2>;
+render(
+  <App />,
+  node
+);
