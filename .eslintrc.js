83205608c8d7ee2030439f9b16e6dcc3622a52aa
Kushan Joshi <0o3ko0@gmail.com>
2016-05-26 16:01:46 +0530

    packages update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,14 @@
 /*eslint-disable*/
 module.exports = {
   'extends': 'airbnb',
-  'parser': 'babel-eslint'
+  'parser': 'babel-eslint',
+  rules: {
+    'max-len': [
+      1,
+      240,
+      2, {
+        ignoreComments: true
+      }
+    ],
+  }
 };
